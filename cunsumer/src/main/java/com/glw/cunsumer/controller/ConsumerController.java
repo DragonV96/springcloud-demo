6d7ae6200ec35642f02cb5988d40a82c1bb2664d
glw <510537547@qq.com>
2019-06-25 17:00:41 +0800

    【添加】添加consummer服务消费者

---
+++ cunsumer/src/main/java/com/glw/cunsumer/controller/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.glw.cunsumer.controller;
+
+import com.glw.cunsumer.feign.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : glw
+ * @date : 2019/6/25
+ * @time : 16:58
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+}
---
+++ cunsumer/src/main/java/com/glw/cunsumer/feign/HelloRemote.java
@@ -0,0 +1,17 @@
+package com.glw.cunsumer.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author : glw
+ * @date : 2019/6/25
+ * @time : 16:57
+ */
+@FeignClient(name = "spring-cloud-producer")
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
